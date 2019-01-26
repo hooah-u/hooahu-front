62de7c3de760a736b2a14d44d11fd27ba03ba875
ShinHyunJong <pleiades@algorithmlabs.co.kr>
2019-01-26 19:26:35 +0900

    서버 주소 업데이트

--- src/Configs/Server.js
+++ src/Configs/Server.js
@@ -3,7 +3,6 @@
  * Email: nayunhwan.dev@mgail.com
  */
 
-export const ServerEndPoint =
-  "http://ec2-13-125-63-20.ap-northeast-2.compute.amazonaws.com:3000/";
+export const ServerEndPoint = "https://api.hooahu.com/";
 
 // export const ServerEndPoint = "http://localhost:3000/";
