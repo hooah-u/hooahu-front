b38b5e97caf44214d1592d8120b4b4ad65b2fbd3
ShinHyunJong <pleiades@algorithmlabs.co.kr>
2019-01-27 14:20:28 +0900

    태그페이지 오류 수정

--- src/Pages/TagPage/TagPage.js
+++ src/Pages/TagPage/TagPage.js
@@ -278,6 +278,7 @@ class TagPage extends Component {
                   <Post
                     isTag
                     feed={data}
+                    user={user}
                     key={index}
                     index={index}
                     onClickTag={this.handleTag}
